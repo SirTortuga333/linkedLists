09f03c0b4561b701e847d8430ff8161326ed33f5
Gino Fabrizio <51064212+SirTortuga333@users.noreply.github.com>
2025-05-07 22:14:34 -0300

    Added function append, prepend, head, toString and tested them

--- linkedList.js
+++ linkedList.js
@@ -1,16 +1,40 @@
-import { Node } from "./node";
+import { Node } from "./node.js";
 export class LinkedList {
+  headNode = null;
+
   append(value) {
     // Adds to end of list
+    if (this.head() === null) {
+      const node = new Node();
+      node.value = value;
+      this.headNode = node;
+      return;
+    }
+
+    let tmp = this.head();
+    while (tmp.nextNode !== null) tmp = tmp.nextNode;
+    const node = new Node();
+    node.value = value;
+    tmp.nextNode = node;
+    return;
   }
   prepend(value) {
     // Adds to begining of list
+    if (this.head() === null) this.append(value);
+    else {
+      const node = new Node();
+      node.value = value;
+      node.nextNode = this.head();
+      this.headNode = node;
+    }
+    return;
   }
   size() {
     // returns size of list (counts nodes)
   }
   head() {
     // returns first node
+    return this.headNode;
   }
   tail() {
     // returns last node
@@ -28,13 +52,24 @@ export class LinkedList {
     // returns index if founds value on list
   }
   toString() {
-    // represents your LinkedList objects as strings, so you can print them out and preview them in the console. 
+    // represents your LinkedList objects as strings, so you can print them out and preview them in the console.
     // The format should be: ( value ) -> ( value ) -> ( value ) -> null
+
+    if (this.head() === null) return "Empty Linked List.";
+
+    let string = "";
+    let tmp = this.head();
+    while (tmp !== null) {
+      string += `( ${tmp.value} ) -> `;
+      tmp = tmp.nextNode;
+    }
+    if (tmp === null) string += "null";
+    return string;
   }
-  insertAt(value, index){
+  insertAt(value, index) {
     // inserts a node with "value" at "index"
   }
-  removeAt(index){
+  removeAt(index) {
     // removes node at "index"
   }
 }
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-import { LinkedList } from "./linkedList";
+import { LinkedList } from "./linkedList.js";
 
 // example uses class syntax - adjust as necessary
 const list = new LinkedList();
 
-list.append("dog");
+list.prepend("dog");
 list.append("cat");
 list.append("parrot");
 list.append("hamster");
 list.append("snake");
-list.append("turtle");
+list.prepend("turtle");
 
 console.log(list.toString());
